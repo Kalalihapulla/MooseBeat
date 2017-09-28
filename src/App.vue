665d2c7dff206227838540d61480d0a5169f9d18
Himel <rebelhaze@gmail.com>
2017-09-28 20:16:54 +0300

    w

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,44 @@
 <template>
   <div id="app">
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+    <b-navbar class="navbar fixed-top" toggleable="md" type="dark" variant="info">
 
-      <div class="container">
-        
-        <div><router-link class="navbar-brand" v-bind:to="'/'">MooseBeat</router-link></div>
-        
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+      <b-nav-toggle target="nav_collapse"></b-nav-toggle>
 
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <router-link class="nav-link" v-bind:to="'/profile'">Profile</router-link>
-            </li>
-             <li class="nav-item">
-              <router-link class="nav-link" v-bind:to="'/artist'">Artist</router-link>
-            </li>
-          
-          
-          </ul>
-        </div>
-      </div>
-    </nav>
+      <b-navbar-brand router-link v-bind:to="'/'">MooseBeat</b-navbar-brand>
+
+      <b-collapse is-nav id="nav_collapse">
+
+        <!-- Right aligned nav items -->
+        <b-nav is-nav-bar class="ml-auto">
+          <b-nav is-nav-bar>
+            <b-nav-item router-link v-bind:to="'/artist'">Artist</b-nav-item>
+            <b-nav-item router-link v-bind:to="'/profile'">Profile</b-nav-item>
+          </b-nav>
+          <b-nav-form>
+            <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search" />
+            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+          </b-nav-form>
+
+          <b-nav-item-dropdown right>
+            <!-- Using button-content slot -->
+            <template slot="button-content">
+              <em>User {{username}}</em>
+            </template>
+            <div v-if="this.username != null">
+              <b-dropdown-item router-link v-bind:to="'/profile'">Profile</b-dropdown-item>
+              <b-dropdown-item a href="/logout">Logout</b-dropdown-item>
+            </div>
+            <div v-else>
+              <b-dropdown-item router-link v-bind:to="'/profile'">Login</b-dropdown-item>
+            </div>
+
+       
+          </b-nav-item-dropdown>
+        </b-nav>
+
+      </b-collapse>
+    </b-navbar>
     <!-- the router outlet, where all matched components would ber viewed -->
 
     <!--  <router-link v-bind:to="'/about'">About</router-link> -->
