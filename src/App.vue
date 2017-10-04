c9b259ddd0e023b185a6f3f04c3e501478273086
NgocNguyen95 <pandadota29@gmail.com>
2017-10-05 02:12:33 +0300

    Landing page

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,29 @@
 <template>
   <div id="app">
 
-<!--    <section class="home-wrapper">
+    <div class="home-wrapper">
       <div class="home-back">
         <img class="back-img" src="/static/img/background-home.jpg" style="width:100%">
+        
+        <img class="logo-img" src="/static/img/logo_and_name/logo/icon1-trans.png" style="top:calc(5px + 10vw)">
+        <img class="logo-img" src="/static/img/logo_and_name/name/name1-purple.png" style="top:calc(70px + 25vw)">        
+        <button class="but1" style="top:calc(150px + 30vw)">Explore Our Platform
+          
+        </button>
+        
+        <h1 class="slogan">Keep listen to music!</h1>
       </div>
       
-    </section>
-    -->
-
+    </div>
+    
     <nav class="navbar fixed-top navbar-expand-lg navbar-dark">
 
       <div class="container">
 
         <div>
           <router-link class="navbar-brand" v-bind:to="'/'">
-            <img src="/static/img/logo_and_name/logo/icon1-white.png" style="width:auto; height:30px" class="d-inline-block align-top" alt="">
-            <img src="/static/img/logo_and_name/name/name1-white.png" style="width:auto; height:30px" class="d-inline-block align-top" alt="">
+            <img src="/static/img/logo_and_name/logo/icon1-white.png" style="width:auto; height:30px" class="d-inline-block align-top" >
+            <img src="/static/img/logo_and_name/name/name1-white.png" style="width:auto; height:30px" class="d-inline-block align-top" >
           </router-link>
 
         </div>
@@ -49,17 +56,102 @@
       </div>
     </nav>
 
+    <section class="info">
+      <div>
+        <div>
+          <div class="row">
+            <div class="col-sm-6 col-md-6" style="max-width:100vw">
+              <h2>
+                            What is MooseBeat?
+              </h2>
+              <div>
+                <h3>
+                     Music database / community
+                </h3>
+                <p>
+                                There are millions of songs on Spotify. Play your favorites, discover new tracks, and build the perfect collection.
+                </p>
+              </div>
+              <div>
+                <h3>
+                     Features
+                </h3>
+                <p>
+                    Song recognition, mobile application, music recommendation algorithm, overall information about the music, user reviews, adding your own music, connection with music service providers such as Spotify, YouTube and Soundcloud and more to come
+                </p>
+              </div>
+              <div>
+                <h3>
+                      All deveices friendly
+                </h3>
+                <p>
+                      The product provides independent services for both smart devices and web usage that can be easily connected and used together for the maximum music experience.
+                </p>
+              </div>
+            </div>
+
+            <div class="col-sm-6 col-md-6">
+              <img src="/static/img/Screenshot.png" style="max-width:45vw">
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <footer>
+                
+        <div class="container" style="border-top:1px solid grey;">
+            <div class="row text-center">   
+                <div class="col-lg-6 col-lg-offset-3">
+                      <ul class="menu">
+                                 
+                             <li>
+                                <a href="#">Home</a>
+                              </li>
+                                   
+                              <li>
+                                 <a href="#">About</a>
+                              </li>
+                                   
+                              <li>
+                                <a href="#">Blog</a>
+                              </li>
+                                   
+                              <li>
+                                 <a href="#">Gallery</a>
+                              </li>
+                                   
+                              <li>
+                                <a href="#">Contact</a>
+                             </li>
+                   
+                        </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="copyright">
+         <div class="container">
+           
+             <div class="row text-center">
+              <p>Copyright © 2017 All rights reserved</p>
+             </div>
+             
+         </div>
+        </div>
+        
+    </footer>
+      
    
 
     <!-- the router outlet, where all matched components would ber viewed -->
 
     <!--  <router-link v-bind:to="'/about'">About</router-link> -->
 
-    <div id="render">
+    
+<!--    <div id="render">
       <router-view></router-view>
-    </div>
-
-       
+    </div>  -->    
 
   </div>
 </template>
@@ -106,6 +198,9 @@ export default {
   margin-top: 60px;
 }
 
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">   
+
 body,
 h1,
 h2,
@@ -117,34 +212,73 @@ h6 {
   font-weight: 700;
 }
 
-body {
-  background: url(/static/img/free-purple-triangles-vector.jpg);
-  background-attachment: fixed;
-  background-repeat: no-repeat;
-  background-size: cover;
+.logo-img {
+  position: absolute;
+  right: 15%;  
+  width: calc(100px + 10vw);
+  height: auto;
+  max-width: 300px;
+}
+
+.slogan {
+  position: absolute;  
+  left: 5%;
+  top: 15%;
+  font-size: calc(40px + 5vw);
+  max-width: 10px;
+  color: white;
+}
+
+.but1 {
+  -webkit-transition-duration: 0.4s; /* Safari */
+  transition-duration: 0.4s;
+  position: absolute;  
+  padding: 16px 32px;
+  text-align: center;
+  text-decoration: none;
+  background-color: transparent;
+  color: #9C27B0;
+  border-radius: 12px;
+  border: 2px solid #9C27B0;
+  cursor: pointer;
+  width: calc(130px + 15vw);
+  font-size: calc(12px + 1vw);
+  right: 12vw; 
+}
+
+.but1:hover {
+  background-color: #9C27B0;
+  color: white;
+}
+
+.info{
+  padding-top: 5vw;
+  width: 100%;
+  position: absolute;
+  top: 55vw;
+  background-color: #F9F9F9;
 }
 
 .navbar {   
-  background-color:  #9C27B0;  
+  background-color:  transparent;  
   border-bottom-style: solid;
   border-bottom-color: rgb(239, 241, 242);
   border-bottom-width: 0;
   box-shadow: none;
 }
-<!--.navbar:hover {
+.navbar:hover {
   display: block;  
   background-color: #9C27B0;  
   transition-property: all;
   transition-duration: 0.3s;
   transition-timing-function: ease-out;
   transition-delay: initial;
-}-->
+}
 
 .home-back { 
   position: absolute;
   width: 100%;
-  height: auto;  
-  box-shadow: 1px 1px 23px 0 rgba(0,0,0,.6);
+  height: auto;
 }
 
 .home-wrapper {
@@ -285,6 +419,67 @@ body {
 .navbar-expand .navbar-nav .show .dropdown-menu .dropdown-item.active {
   color: #a7dff4;
   background-color: #0091c5;
+}
+
+footer{
+  position: absolute;
+  top: 100vw;
+  padding: 4rem 0;
+  margin-top: 4rem;
+  font-size: 85%;
+  text-align: center;
+  background-color: #9C27B0;
+  width: 100%;
+  font-color: white;
+}
+
+footer ul{
+    list-style:none;
+    margin:0 auto;
+    display:inline-block;
+    padding-top:50px;
+}
+
+footer ul li{
+    float:left;
+}
+
+footer ul li a{
+  color:#b1aca1;
+    padding:20px;
+}
+
+footer ul li a:hover{
+  color:#7dabdb;
+  text-decoration:none;
+}
+
+.footer-top{
+  padding-bottom:50px;
+}
+
+.footer-top .fa{
+    font-size:18px;
+    color: #FFF;
+    padding-right:10px;
+}
+
+.footer-top a{
+    color:#FFF;
+    text-decoration:none;
+}
+
+.footer-top .col-lg-2:hover .fa , .footer-top .col-lg-2:hover a{
+    color:#7dabdb;
+}
+
+.copyright{
+    background-color:#f0f0f0;
+}
+
+.copyright p{
+  font-size:12px;
+  padding:15px 0 10px 0;
 }
 
 #render {}
