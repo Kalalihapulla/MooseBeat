09d8771dcaf3e53b2d9553a11d6cdfadce91a8d3
Himel <rebelhaze@gmail.com>
2017-10-08 21:13:15 +0300

    Artist bio

--- src/routes/artistRoutes.js
+++ src/routes/artistRoutes.js
@@ -19,7 +19,20 @@ router.get('/artists/:query/', function (req, res) {
   request.get({ url: "http://api.musicgraph.com/api/v2/artist/suggest?api_key=121e2a1c43b279d6e8e80c1b3ed2e04d&prefix=" + req.params.query + "&limit=10" }, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       console.log(req.params.query);
- 
+
+      res.send(body);
+
+    }
+  });
+
+
+});
+router.get('/artists/bio/:mbid', function (req, res) {
+
+  request.get({ url: "http://api.openaura.com/v1/info/artists/"+req.params.mbid+"?id_type=musicbrainz%3Agid&api_key=dabdfde06848e9adabe05b1862d4e0c4e84a3684" }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(req.params.query);
+
       res.send(body);
 
     }
